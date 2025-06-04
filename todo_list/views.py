3c8f57020755bef535e8a304a2342405ccc0da3a
Hugo Loiola <hugoloiola.1@gmail.com>
2025-06-04 10:56:52 -0300

    feat: forms

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def home(request):
-    todos = Todo.objects.all()
-    return render(request, 'home.html', {'todos': todos})
+    if request.method == 'POST':
+        form = TodoForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            todos = Todo.objects.all()
+            return render(request, 'home.html', {'todos': todos})
+        else:
+            todos = Todo.objects.all()
+            return render(request, 'home.html', {'todos': todos})
 
 def about(request):
     context = {'name': 'Hugo Loiola', 'age': '22'}
